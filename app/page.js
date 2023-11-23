4c8b045a905db9c8f0de2d5fc02e96ec5cf1b19e
Varsani2520 <varsaniranjani@gmail.com>
2023-11-23 13:51:20 +0530

    f

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ const page = () => {
           <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={50}
-            slidesPerView={3}
+            slidesPerView={1}
             navigation
             pagination={{ clickable: true }}
             scrollbar={{ draggable: true }}
@@ -34,18 +34,18 @@ const page = () => {
             onSwiper={(swiper) => console.log(swiper)}
           >
             <SwiperSlide>
-              <img src="https://media.istockphoto.com/id/1425540760/photo/young-woman-choosing-clothes-in-wardrobe-closet-back-view.jpg?s=612x612&w=0&k=20&c=FU5_CX985u7URA86hziqh8BW7u6SiV_WfVrXbEWZ2tw=" />
+              <img src="https://media.istockphoto.com/id/1425540760/photo/young-woman-choosing-clothes-in-wardrobe-closet-back-view.jpg?s=612x612&w=0&k=20&c=FU5_CX985u7URA86hziqh8BW7u6SiV_WfVrXbEWZ2tw=" width={1000}/>
             </SwiperSlide>
             <SwiperSlide>
-              <img src="https://media.istockphoto.com/id/931577634/photo/soft-focus-of-a-two-years-old-child-choosing-her-own-dresses-from-kids-cloth-rack.jpg?s=612x612&w=0&k=20&c=VYPxAfk5AODBbX6DW5RTZ1v3R2PD4H2-1e9fa9OAcM0=" />
+              <img src="https://media.istockphoto.com/id/931577634/photo/soft-focus-of-a-two-years-old-child-choosing-her-own-dresses-from-kids-cloth-rack.jpg?s=612x612&w=0&k=20&c=VYPxAfk5AODBbX6DW5RTZ1v3R2PD4H2-1e9fa9OAcM0=" width={1000}/>
             </SwiperSlide>
             <SwiperSlide>
-              <img src="https://media.istockphoto.com/id/1131600024/photo/women-buying-earring-at-street-market.jpg?s=612x612&w=0&k=20&c=WBzRRkzmpwMwxH3TIiNCZGMcMe00QwNWiFzo3b5WCvE=" />
+              <img src="https://media.istockphoto.com/id/1131600024/photo/women-buying-earring-at-street-market.jpg?s=612x612&w=0&k=20&c=WBzRRkzmpwMwxH3TIiNCZGMcMe00QwNWiFzo3b5WCvE=" width={1000}/>
             </SwiperSlide>
             <SwiperSlide>
-              <img src="https://media.istockphoto.com/id/1480208626/photo/childrens-room-interior-with-table-cute-paintings.jpg?s=612x612&w=0&k=20&c=HQo5rKhYAPeXWzYMa3FLOIUC8d9UHFkEA3270U1dE30=" />
+              <img src="https://media.istockphoto.com/id/1480208626/photo/childrens-room-interior-with-table-cute-paintings.jpg?s=612x612&w=0&k=20&c=HQo5rKhYAPeXWzYMa3FLOIUC8d9UHFkEA3270U1dE30=" width={1000}/>
             </SwiperSlide>
-            ...
+            
           </Swiper>
         </Grid>
       </Grid>
