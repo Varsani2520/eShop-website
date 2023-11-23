42bdeac07ca1dfafde9b610e9f492be5e48fbb39
Varsani2520 <varsaniranjani@gmail.com>
2023-11-23 11:32:56 +0530

    routes

--- app/api/get-faviorite/route.js
+++ app/api/get-faviorite/route.js
@@ -1,20 +1,11 @@
 import { connectDatabase } from "@/app/database/db";
-import { faviorite } from "@/app/modal/get-faviourite";
 
 import { NextResponse } from "next/server";
 
 connectDatabase();
 export async function POST(request) {
-  const { token } = await request.json();
-
-  try {
-    const getFav = new faviorite({
-      token: token,
-    });
-    const faviorites = await getFav.save();
-    console.log(faviorite);
+  
     return NextResponse.json({ msg: "success" });
-  } catch (error) {
-    return NextResponse.json({ msg: "fail" });
+  
   }
-}
+
