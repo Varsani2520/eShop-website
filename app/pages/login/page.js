851027372d4251508c104566fc2351ed2cf802ea
Varsani2520 <varsaniranjani@gmail.com>
2023-11-29 12:18:20 +0530

    pages

--- app/pages/login/page.js
+++ app/pages/login/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import LoginPage from "../../components/loginPage";
+import LoginPage from "../../components/LoginPage";
 
 const page = () => {
   return (
--- app/pages/profile/page.js
+++ app/pages/profile/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Lottie from "lottie-react";
 import React from "react";
-import emptyProfile from '../../lottie-animation/emptyProfile'
+import emptyProfile from '../../lottie-animation/emptyProfile.json'
 const page = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}>
