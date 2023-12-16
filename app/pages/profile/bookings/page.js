efcb8904215501592158c20670b2dd8e745f0d15
Varsani2520 <varsaniranjani@gmail.com>
2023-12-16 18:52:18 +0530

    final

--- app/pages/profile/bookings/page.js
+++ app/pages/profile/bookings/page.js
@@ -1,23 +1,40 @@
 "use client"
-import React from "react";
-import { useRouter } from "next/navigation";
+import { summaryServices } from "@/app/service/summary";
+import { Card } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const page = () => {
-  const router = useRouter();
+  const tokens = useSelector((state) => state.auth.authUser.data.token);
   const carts = useSelector((state) => state.cart.cartItems);
+  const date = new Date();
+  const [booking, setBooking] = useState(null);
 
-  
+  async function Booking() {
+    try {
+      const response = await summaryServices(tokens, carts, "pending", date);
+      setBooking(response);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    Booking();
+  }, []);
 
   return (
     <div>
-      <h1>Your Order is Complete!</h1>
-      <h2>Ordered Items:</h2>
-      <ul>
-        {carts.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      <h1>Your Bookings !</h1>
+      {booking && (
+        <div>
+          <Card>
+            <h1>Order Date:{booking.date}</h1>
+            <h1>Status:{booking.status}</h1>
+          </Card>
+        </div>
+      )}
     </div>
   );
 };
