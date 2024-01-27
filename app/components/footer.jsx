e337070605324a4e3ecb1424894768542d7cc2bb
Varsani2520 <varsaniranjani@gmail.com>
2024-01-27 12:57:08 +0530

    final

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,33 +1,23 @@
-import React from "react";
-import { Container, Grid, Typography } from "@mui/material";
-import Link from "@mui/material/Link";
+import React from 'react';
+import { Container, Grid, Typography } from '@mui/material';
+import Link from '@mui/material/Link';
 
 const footer = () => {
   return (
-    <footer
-      style={{ padding: "20px", backgroundColor: "#595959", color: "white" }}
-    >
+    <footer style={{ background: 'hotpink', padding: '20px' }}>
       <Container maxWidth="lg">
         <Grid container spacing={3}>
           {/* Left Section: About */}
           <Grid item xs={12} sm={4}>
-            <Typography variant="h2" color="textPrimary" gutterBottom>
+            <Typography variant="h6" color="textPrimary" gutterBottom>
               About
             </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              sx={{ fontWeight: "bold", padding: "5px" }}
-            >
+            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
               <Link href="/pages/contact" color="inherit">
                 Contact Us
               </Link>
             </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              sx={{ fontWeight: "bold", padding: "5px" }}
-            >
+            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
               <Link href="/pages/about" color="inherit">
                 About Us
               </Link>
@@ -36,46 +26,30 @@ const footer = () => {
 
           {/* Center Section: Address */}
           <Grid item xs={12} sm={4}>
-            <Typography variant="h2" color="textPrimary" gutterBottom>
+            <Typography variant="h6" color="textPrimary" gutterBottom>
               Mail Us
             </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              sx={{ fontWeight: "bold", padding: "5px" }}
-            >
+            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
               Bhuj,Gujarat,ABC area, 370030
             </Typography>
           </Grid>
 
           {/* Right Section: Social */}
           <Grid item xs={12} sm={4}>
-            <Typography variant="h2" color="textPrimary" gutterBottom>
+            <Typography variant="h6" color="textPrimary" gutterBottom>
               Social
             </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              sx={{ fontWeight: "bold", padding: "5px" }}
-            >
+            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
               <Link href="https://www.facebook.com" color="inherit">
                 Facebook
               </Link>
             </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              sx={{ fontWeight: "bold", padding: "5px" }}
-            >
+            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
               <Link href="https://www.twitter.com" color="inherit">
                 Twitter
               </Link>
             </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              sx={{ fontWeight: "bold", padding: "5px" }}
-            >
+            <Typography variant="body2" color="textSecondary" sx={{ color: 'white', fontWeight: 'bold', padding: '5px' }}>
               <Link href="https://www.linkedin.com" color="inherit">
                 LinkedIn
               </Link>
