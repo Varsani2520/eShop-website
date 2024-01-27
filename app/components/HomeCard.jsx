4040b0b9f471ae26c23109aca74774de443b99ef
Varsani2520 <varsaniranjani@gmail.com>
2024-01-27 13:33:36 +0530

    img

--- app/components/HomeCard.jsx
+++ app/components/HomeCard.jsx
@@ -37,7 +37,8 @@ const HomeCard = () => {
   return (
     <>
       <Container>
-        <ToastContainer position="top-center"
+        <ToastContainer
+          position="top-center"
           autoClose={5000}
           hideProgressBar={false}
           newestOnTop={false}
@@ -46,64 +47,65 @@ const HomeCard = () => {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          style={toastStyle} />
+          style={toastStyle}
+        />
         <Box sx={{ display: "flex" }}>
           <Grid container spacing={2}>
             {loading
               ? Array.from({ length: 6 }).map((_, index) => (
-                <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-                  <Box>
-                    <Card sx={{ maxWidth: 345 }}>
-                      <Skeleton
-                        variant="rectangular"
-                        height={194}
-                        animation="wave"
-                      />
-                      <CardContent>
-                        <Skeleton animation="wave" />
-                      </CardContent>
-                    </Card>
-                    <br />
-                  </Box>
-                </Grid>
-              ))
+                  <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
+                    <Box>
+                      <Card sx={{ maxWidth: 345 }}>
+                        <Skeleton
+                          variant="rectangular"
+                          height={194}
+                          animation="wave"
+                        />
+                        <CardContent>
+                          <Skeleton animation="wave" />
+                        </CardContent>
+                      </Card>
+                      <br />
+                    </Box>
+                  </Grid>
+                ))
               : card.map((response) => (
-                <Grid item xs={12} sm={6} md={6} lg={4} key={response.id}>
-                  <Box>
-                    <Card sx={{
-                      maxWidth: "100%",
-                      transition: "transform 0.3s ease-in-out",
-                      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                      "&:hover": {
-                        transform: "scale(1.05)",
-                        boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)",
-                      },
-                    }}
-                    >
-                      <CardHeader
-                        title={response.title}
-                        sx={{ background: "#d4d5ee" }}
-                      />
-                      <CardMedia
-                        component="img"
-                        image={response.image}
-                        alt={response.alt}
-                        sx={{ cursor: "pointer", objectFit: 'cover' }}
-                        onClick={() =>
-                          router.push(`${response.id}/${response.slug}`)
-                        }
-                      />
-                      <CardContent>
-                        <Typography variant="body2" color="text.secondary">
-                          {response.description}
-                        </Typography>
-                      </CardContent>
-
-                    </Card>
-                    <br />
-                  </Box>
-                </Grid>
-              ))}
+                  <Grid item xs={12} sm={6} md={6} lg={4} key={response.id}>
+                    <Box>
+                      <Card
+                        sx={{
+                          maxWidth: "100%",
+                          transition: "transform 0.3s ease-in-out",
+                          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                          "&:hover": {
+                            transform: "scale(1.05)",
+                            boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)",
+                          },
+                        }}
+                      >
+                        <CardHeader
+                          title={response.title}
+                          sx={{ background: "#d4d5ee" }}
+                        />
+                        <CardMedia
+                          component="img"
+                          image={response.image}
+                          alt={response.alt}
+                          sx={{ cursor: "pointer", objectFit: "cover" }}
+                          onClick={() =>
+                            router.push(`${response.id}/${response.slug}`)
+                          }
+                        />
+                        <CardContent>
+                          <Typography variant="body2" color="text.secondary">
+                            {response.description}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                      <br />
+                    </Box>
+                  </Grid>
+                ))}
           </Grid>
         </Box>
       </Container>
--- app/components/Slider1.jsx
+++ app/components/Slider1.jsx
@@ -1,8 +1,7 @@
-"use client"
-import React from "react";
-import { Grid } from "@mui/material";
-import Container from "@mui/material/Container/Container";
-import style from '../styles/style.css'
+import React from 'react';
+import { Grid } from '@mui/material';
+import Container from '@mui/material/Container';
+import style from '../styles/style.css';
 
 const Slider1 = () => {
   return (
@@ -10,32 +9,40 @@ const Slider1 = () => {
       <Container>
         <Grid container>
           <Grid item xs={12} md={6}>
-            <img
-              src="https://img.freepik.com/free-vector/kids-toys-design_24908-56648.jpg?size=626&ext=jpg&ga=GA1.1.248855276.1696004271&semt=ais"
-              alt="img"
-              style={{ width: '100%',height:'100%' }} // Full width on all screen sizes
-            />
+            <div className="image-container">
+              <img
+                src="https://img.freepik.com/free-vector/kids-toys-design_24908-56648.jpg?size=626&ext=jpg&ga=GA1.1.248855276.1696004271&semt=ais"
+                alt="img"
+                className="image"
+              />
+            </div>
           </Grid>
           <Grid item xs={12} md={6}>
-            <img
-              src="https://img.freepik.com/free-vector/toy-store-cartoon-landing-page-shop-showcase_107791-14657.jpg?size=626&ext=jpg&uid=R121733695&ga=GA1.1.248855276.1696004271&semt=ais"
-              alt="img"
-              style={{ width: '100%' ,height:'100%'}} // Full width on all screen sizes
-            />
+            <div className="image-container">
+              <img
+                src="https://img.freepik.com/free-vector/toy-store-cartoon-landing-page-shop-showcase_107791-14657.jpg?size=626&ext=jpg&uid=R121733695&ga=GA1.1.248855276.1696004271&semt=ais"
+                alt="img"
+                className="image"
+              />
+            </div>
           </Grid>
           <Grid item xs={12} md={6}>
-            <img
-              src="https://img.freepik.com/free-psd/banner-template-with-cooking_23-2148543090.jpg"
-              alt="img"
-              style={{ width: '100%' }} // Full width on all screen sizes
-            />
+            <div className="image-container">
+              <img
+                src="https://img.freepik.com/free-psd/banner-template-with-cooking_23-2148543090.jpg"
+                alt="img"
+                className="image"
+              />
+            </div>
           </Grid>
           <Grid item xs={12} md={6}>
-            <img
-              src="https://img.freepik.com/free-psd/new-house-real-estate-landing-page_23-2148855243.jpg"
-              alt="img"
-              style={{ width: '100%' }} // Full width on all screen sizes
-            />
+            <div className="image-container">
+              <img
+                src="https://img.freepik.com/free-psd/new-house-real-estate-landing-page_23-2148855243.jpg"
+                alt="img"
+                className="image"
+              />
+            </div>
           </Grid>
         </Grid>
       </Container>
