03673657794798a31d5e4e6a55a82978fd0d4d0d
Varsani2520 <varsaniranjani@gmail.com>
2024-01-27 13:51:30 +0530

    metadataa

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,10 @@ import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
-  title: "erequirements",
+  title: {
+    default: "eShop",
+    template: "%s | eShop",
+  },
 };
 
 export default function RootLayout({ children }) {
